1e70dfdef6ef0485a9bca099ae8959b9c62f282c
George Baptista <george@omame.com>
2020-10-01 20:55:48 +0900

    chp09-set933

---
+++ chp09-deployments/set933-rollback/README.md
@@ -0,0 +1,6 @@
+# Chapter 9, Section 9.3.3
+
+### Objective
+
+### Notes
+
---
+++ chp09-deployments/set933-rollback/TEST.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+. ~/src/SETUP.sh
+FULLPATH=$(pwd)
+echo $HR_TOP
+
+kubectl apply -f $FULLPATH/set933-0-ns.yaml
+kubectl apply -f $FULLPATH/set933-1-kubia-deployment-v1.yaml --record
+kubectl apply -f $FULLPATH/set933-2-service.yaml
+sleep 1
+echo $HR
+
+
+echo "Check ReplicaSet pod-template-hash"
+echo "kubectl get rs -n=chp09-set933 -o jsonpath={'.items[0].metadata.labels.pod-template-hash'}"
+kubectl get rs -n=chp09-set933 -o jsonpath={'.items[0].metadata.labels.pod-template-hash'}
+echo ""
+echo $HR
+
+echo "kubectl get rs -n=chp09-set933 -o wide --show-labels"
+kubectl get rs -n=chp09-set933 -o wide --show-labels
+echo $HR
+
+
+echo "kubectl get pods -n=chp09-set933 --show-labels"
+kubectl get pods -n=chp09-set933 --show-labels
+echo $HR
+
+#echo "Wait for a pod to start up"
+echo "POD_0=\$(kubectl get pod -n=chp09-set933 -o jsonpath={'.items[0].metadata.name'})"
+POD_0=$(kubectl get pod -n=chp09-set933 -o jsonpath={'.items[0].metadata.name'})
+#echo "POD_0 is $POD_0"
+#echo ""
+
+
+echo "kubectl wait --for=condition=Ready=True pod/$POD_0 -n=chp09-set933 --timeout=21s"
+kubectl wait --for=condition=Ready=True pod/$POD_0 -n=chp09-set933 --timeout=21s
+echo ""
+
+SVC_IP=$(kubectl get svc/kubia -n=chp09-set933 -o jsonpath={'.status.loadBalancer.ingress[0].ip'})
+#echo "SVC_IP is $SVC_IP"
+
+echo $HR
+
+echo "curl $SVC_IP"
+counter=1
+while [ $counter -le 10 ]
+do
+  curl http://$SVC_IP 
+  sleep 0.7
+  ((counter++))
+done
+echo ""
+
+echo $HR
+
+echo "kubectl rollout status deployment kubia -n=chp09-set933"
+kubectl rollout status deployment kubia -n=chp09-set933
+echo ""
+
+
+#echo "kubectl patch deployment kubia -p '{\"spec\": {\"minReadySeconds\": 3}}' -n=chp09-set933"
+#kubectl patch deployment kubia -p '{"spec": {"minReadySeconds": 3}}' -n=chp09-set933
+#echo ""
+
+# Imperative style
+#echo "kubectl set image deployment kubia nodejs=luksa/kubia:v2 -n=chp09-set933"
+#kubectl set image deployment kubia nodejs=luksa/kubia:v2 -n=chp09-set933
+#echo ""
+
+echo $HR
+
+# Declarative style
+echo "kubectl apply -f $FULLPATH/set933-3-kubia-deployment-v3.yaml --record"
+kubectl apply -f $FULLPATH/set933-3-kubia-deployment-v3.yaml --record
+echo ""
+
+echo "Rollout to BAD deployment:"
+echo "kubectl rollout status deployment kubia -n=chp09-set933"
+kubectl rollout status deployment kubia -n=chp09-set933
+echo ""
+
+echo "kubectl get rs -n=chp09-set933 -o wide"
+kubectl get rs -n=chp09-set933 -o wide
+echo $HR
+
+echo "kubectl get pods -n=chp09-set933 --show-labels"
+kubectl get pods -n=chp09-set933 --show-labels
+echo $HR
+
+echo "curl $SVC_IP"
+counter=1
+while [ $counter -le 30 ]
+do
+  curl http://$SVC_IP 
+  sleep 0.7
+  ((counter++))
+done
+echo $HR
+
+echo "Rollback to OK deployment:"
+echo "kubectl rollout undo deployment kubia --to-revision=1 -n=chp09-set933"
+kubectl rollout undo deployment kubia --to-revision=1 -n=chp09-set933
+echo $HR
+
+echo "kubectl rollout history deployment kubia -n=chp09-set933"
+kubectl rollout history deployment kubia -n=chp09-set933
+echo $HR
+
+echo "curl $SVC_IP"
+counter=1
+while [ $counter -le 30 ]
+do
+  curl http://$SVC_IP 
+  sleep 0.7
+  ((counter++))
+done
+echo $HR
+
+
+echo "kubectl delete ns chp09-set933"
+kubectl delete ns chp09-set933
