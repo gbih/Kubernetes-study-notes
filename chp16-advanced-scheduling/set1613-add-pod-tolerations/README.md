f3c82762c23939b63ebf5292145dde5dbf592ba3
George Baptista <george@omame.com>
2020-10-15 15:05:19 +0900

    set1613

--- chp16-advanced-scheduling/set1613-add-pod-tolerations/README.md
+++ chp16-advanced-scheduling/set1613-add-pod-tolerations/README.md
@@ -1,6 +1,7 @@
 # Chapter 16, Section 16.1.3
 
 ### Objective
+- Deploy production pods to production nodes. Need to tolerate taints added to nodes.
 
 ### Notes
 
--- chp16-advanced-scheduling/set1613-add-pod-tolerations/TEST.sh
+++ chp16-advanced-scheduling/set1613-add-pod-tolerations/TEST.sh
@@ -4,13 +4,20 @@ FULLPATH=$(pwd)
 echo "Adding tolerations to pods"
 echo $HR_TOP
 
-# kubectl get nodes -l name=node1-vm
+# Control-plane node
+echo "Control-plane node"
+echo "kubectl taint node actionbook-vm node-type=master:NoSchedule"
+kubectl taint node actionbook-vm node-type=master:NoSchedule
+echo ""
+
 # Production node
+echo "Production node"
 echo "kubectl taint node -l name=node1-vm node-type=production:NoSchedule"
 kubectl taint node -l name=node1-vm node-type=production:NoSchedule
 echo ""
 
 # Development node
+echo "Development node"
 echo "Also need to taint the non-production pods with a taint"
 echo "kubectl taint node -l name=node2-vm node-type=non-production:NoSchedule"
 kubectl taint node -l name=node2-vm node-type=non-production:NoSchedule
@@ -23,18 +30,33 @@ echo ""
 
 echo "kubectl label node -l name=node2-vm env=development"
 kubectl label node -l name=node2-vm env=development
+echo ""
 
 echo "kubectl label node -l name=actionbook-vm env=control-plain"
 kubectl label node -l name=actionbook-vm env=control-plain
-echo $HR
+
+enter
 
 echo "kubectl apply -f $FULLPATH"
 kubectl apply -f $FULLPATH
 
+enter
+
+echo "Getting taints, style 1"
+echo "kubectl get nodes -o json | jq '.items[].spec.taints'"
+kubectl get nodes -o json | jq '.items[].spec.taints'
 echo $HR
 
-echo "kubectl wait --for=condition=Ready=True pods/curl-restrictive -n=chp15-set1512 --timeout=10s"
-kubectl wait --for=condition=Ready=True pods/curl-restrictive -n=chp15-set1512 --timeout=10s
+echo "Alternative 2"
+echo "kubectl get nodes -o=custom-columns=NAME:.metadata.name,TAINTS:.spec.taints,LABEL-NAME:.metadata.labels.name,LABEL-ENV:.metadata.labels.env"
+kubectl get nodes -o=custom-columns=NAME:.metadata.name,TAINTS:.spec.taints,LABEL-NAME:.metadata.labels.name,LABEL-ENV:.metadata.labels.env
+
+enter
+
+
+
+echo "kubectl wait --for=condition=Ready=True pods/curl-restrictive -n=chp16-set1613 --timeout=10s"
+kubectl wait --for=condition=Ready=True pods/curl-restrictive -n=chp16-set1613 --timeout=10s
 
 echo $HR
 
@@ -44,24 +66,47 @@ echo $HR
 
 enter
 
-echo "kubectl rollout status deployment kubia-deploy -n=chp15-set1512"
-kubectl rollout status deployment kubia-deploy -n=chp15-set1512
+echo "kubectl rollout status deployment kubia-deploy -n=chp16-set1613"
+kubectl rollout status deployment kubia-deploy -n=chp16-set1613
 
 echo $HR
 
-echo "Check PSP is set-up correctly by getting HPA from API server."
+echo "Check PSP is set-up correctly by accessing API server."
 echo "We use RoleBinding with Role resource and ServiceAccount"
 echo ""
-echo "kubectl exec -it curl-restrictive -n=chp15-set1512 -- curl localhost:8001/apis/autoscaling/v1/namespaces/chp15-set1512/horizontalpodautoscalers/kubia | jq 'del(.metadata.managedFields, .metadata.annotations, .metadata.apiVersion)'"
+
+echo "kubectl exec -it curl-restrictive -n chp16-set1613 -- curl localhost:8001/api/v1/namespaces/chp16-set1613/services | jq 'del(.metadata.uid, .items[].metadata.managedFields, .items[].metadata.annotations, .status)' | yq r -P -"
 echo ""
 
-kubectl exec -it curl-restrictive -n=chp15-set1512 -- curl localhost:8001/apis/autoscaling/v1/namespaces/chp15-set1512/horizontalpodautoscalers/kubia | jq 'del(.metadata.managedFields, .metadata.annotations, .metadata.apiVersion)'
+kubectl exec -it curl-restrictive -n chp16-set1613 -- curl localhost:8001/api/v1/namespaces/chp16-set1613/services | jq 'del(.metadata.uid, .items[].metadata.managedFields, .items[].metadata.annotations, .status)' | yq r -P -
+
 
 echo $HR
 
-echo "Press enter to delete objects"
+echo "Press enter to delete taints and objects"
 
 enter
+echo "Remove taints"
+echo "kubectl taint node actionbook-vm node-type=master:NoSchedule-"
+echo "kubectl taint node -l name=node1-vm node-type=production:NoSchedule-"
+echo "kubectl taint node -l name=node2-vm node-type=non-production:NoSchedule-"
+kubectl taint node actionbook-vm node-type=master:NoSchedule-
+kubectl taint node -l name=node1-vm node-type=production:NoSchedule-
+kubectl taint node -l name=node2-vm node-type=non-production:NoSchedule-
+
+echo $HR
+
+echo "Remove labels"
+echo "kubectl label node -l name=node1-vm env-"
+echo "kubectl label node -l name=node2-vm env-"
+echo "kubectl label node -l name=actionbook-vm env-"
+kubectl label node -l name=node1-vm env-
+kubectl label node -l name=node2-vm env-
+kubectl label node -l name=actionbook-vm env-
+
+
+echo $HR
+
 
 echo "kubectl delete -f $FULLPATH"
 kubectl delete -f $FULLPATH --ignore-not-found
