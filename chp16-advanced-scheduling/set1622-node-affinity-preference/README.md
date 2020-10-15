bc1ef1c111e5efac391fbcc2c07e00e28c008caa
George Baptista <george@omame.com>
2020-10-15 16:08:44 +0900

    chp1622 edit start

---
+++ chp16-advanced-scheduling/set1622-node-affinity-preference/README.md
@@ -0,0 +1,9 @@
+# Chapter 16, Section 16.1.3
+
+### Objective
+- Deploy production pods to production nodes. Need to tolerate taints added to nodes.
+
+### Notes
+
+https://kubernetes.io/docs/concepts/scheduling-eviction/taint-and-toleration/
+
---
+++ chp16-advanced-scheduling/set1622-node-affinity-preference/TEST.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+. ~/src/common/setup.sh
+FULLPATH=$(pwd)
+echo "Adding tolerations to pods"
+echo $HR_TOP
+
+# Control-plane node
+echo "Control-plane node"
+echo "kubectl taint node actionbook-vm node-type=master:NoSchedule"
+kubectl taint node actionbook-vm node-type=master:NoSchedule
+echo ""
+
+echo $HR
+
+echo "kubectl label node -l name=node2-vm gpu=true"
+kubectl label node -l name=node2-vm gpu=true
+echo ""
+
+echo "kubectl label node -l name=node1-vm env=production"
+kubectl label node -l name=node1-vm env=production
+echo ""
+
+echo "kubectl label node -l name=node2-vm env=development"
+kubectl label node -l name=node2-vm env=development
+echo ""
+
+echo "kubectl label node -l name=actionbook-vm env=control-plain"
+kubectl label node -l name=actionbook-vm env=control-plain
+
+enter
+
+echo "kubectl apply -f $FULLPATH"
+kubectl apply -f $FULLPATH
+
+enter
+
+echo "Getting taints, style 1"
+echo "kubectl get nodes -o json | jq '.items[].spec.taints'"
+kubectl get nodes -o json | jq '.items[].spec.taints'
+echo $HR
+
+echo "Alternative 2"
+echo "kubectl get nodes -o=custom-columns=NAME:.metadata.name,TAINTS:.spec.taints,LABEL-NAME:.metadata.labels.name,LABEL-ENV:.metadata.labels.env"
+kubectl get nodes -o=custom-columns=NAME:.metadata.name,TAINTS:.spec.taints,LABEL-NAME:.metadata.labels.name,LABEL-ENV:.metadata.labels.env
+
+enter
+
+
+
+echo "kubectl wait --for=condition=Ready=True pods/curl-restrictive -n=chp16-set1622 --timeout=10s"
+kubectl wait --for=condition=Ready=True pods/curl-restrictive -n=chp16-set1622 --timeout=10s
+
+echo $HR
+
+echo "In a separate terminal, run ./WATCH.sh to monitor various objects"
+
+echo $HR
+
+enter
+
+echo "kubectl rollout status deployment kubia-deploy -n=chp16-set1622"
+kubectl rollout status deployment kubia-deploy -n=chp16-set1622
+
+echo $HR
+
+echo "Check PSP is set-up correctly by accessing API server."
+echo "We use RoleBinding with Role resource and ServiceAccount"
+echo ""
+
+echo "kubectl exec -it curl-restrictive -n chp16-set1622 -- curl localhost:8001/api/v1/namespaces/chp16-set1622/services | jq 'del(.metadata.uid, .items[].metadata.managedFields, .items[].metadata.annotations, .status)' | yq r -P -"
+echo ""
+
+kubectl exec -it curl-restrictive -n chp16-set1622 -- curl localhost:8001/api/v1/namespaces/chp16-set1622/services | jq 'del(.metadata.uid, .items[].metadata.managedFields, .items[].metadata.annotations, .status)' | yq r -P -
+
+
+echo $HR
+
+echo "Press enter to delete taints and objects"
+
+enter
+echo "Remove taints"
+echo "kubectl taint node actionbook-vm node-type=master:NoSchedule-"
+kubectl taint node actionbook-vm node-type=master:NoSchedule-
+
+echo $HR
+
+echo "Remove labels"
+echo "kubectl label node -l name=node2-vm gpu-"
+echo "kubectl label node -l name=node1-vm env-"
+echo "kubectl label node -l name=node2-vm env-"
+echo "kubectl label node -l name=actionbook-vm env-"
+kubectl label node -l name=node2-vm gpu-
+kubectl label node -l name=node1-vm env-
+kubectl label node -l name=node2-vm env-
+kubectl label node -l name=actionbook-vm env-
+
+
+echo $HR
+
+
+echo "kubectl delete -f $FULLPATH"
+kubectl delete -f $FULLPATH --ignore-not-found
+
