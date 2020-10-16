d7a80fd888fbd821b4bcb667b6a2ec95920c957d
George Baptista <george@omame.com>
2020-10-16 16:52:27 +0900

    set1722

--- chp17-bestpractices/set1722-crashing-pods/TEST.sh
+++ chp17-bestpractices/set1722-crashing-pods/TEST.sh
@@ -4,6 +4,11 @@ echo "17.2.2 Rescheduling of dead or partially dead pods"
 FULLPATH=$(pwd)
 
 
+echo "kubectl apply -f set1722-0-ns.yaml"
+kubectl apply -f set1722-0-ns.yaml
+
+sleep 1
+
 echo "kubectl apply -f $FULLPATH"
 kubectl apply -f $FULLPATH
 
@@ -18,7 +23,9 @@ kubectl -n=chp17-set1722 get rs crashing-pods -o wide
 echo $HR
 
 echo "No action taken by the ReplicaSet Controller, because current replicas match desired replicas."
-echo ""
+
+enter
+
 echo "kubectl describe rs crashing-pods -n=chp17-set1722"
 kubectl describe rs crashing-pods -n=chp17-set1722
 
@@ -32,10 +39,14 @@ echo "The pod status also shows as running."
 echo ""
 echo "kubectl describe -n=chp17-set1722 pod/$POD0"
 kubectl describe -n=chp17-set1722 pod/$POD0
-echo ""
 
 enter
 
-echo "kubectl delete -f set172-0-ns.yaml"
-kubectl delete -f set172-0-ns.yaml
+echo "kubectl get pods -n=chp17-set1722"
+kubectl get pods -n=chp17-set1722
+
+enter
+
+echo "kubectl delete -f set1722-0-ns.yaml"
+kubectl delete -f set1722-0-ns.yaml
 
