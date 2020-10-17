3e581d0c5f4054fa645a60c0823b350875c96e0e
George Baptista <george@omame.com>
2020-10-17 09:58:19 +0900

    chp18 set1811

---
+++ chp18-extending-k8s/set1811-crd/README.md
@@ -0,0 +1,10 @@
+# 18.1.1 Introducing CustomResourceDefinitions
+
+### Objective
+
+### Notes
+- If a pod's container keeps crashing, the Kubelet will keep restarting it indefinitely.
+- Such pods are not automatically removed and rescheduled, even if they are part of a ReplicaSet.
+- The RS Controller doesn't care if the pod is dead -- all it cares about is the number of pods match the desired replica count.
+
+- Looks like we don't need RBAC/PSP for this object?
---
+++ chp18-extending-k8s/set1811-crd/TEST.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+. ~/src/common/setup.sh
+echo "18.1.1 Introducing CustomResourceDefinitions"
+echo $HR_TOP
+FULLPATH=$(pwd)
+
+echo "kubectl apply -f $FULLPATH/set1811-0-ns.yaml"
+kubectl apply -f $FULLPATH/set1811-0-ns.yaml
+
+echo $HR
+
+echo "Create a CustomResourceDefinition"
+echo ""
+echo "kubectl apply -f $FULLPATH/set1811-1-crd-website.yaml"
+kubectl apply -f $FULLPATH/set1811-1-crd-website.yaml
+echo $HR
+
+echo "kubectl get crd -A -o wide"
+kubectl get crd -A -o wide
+
+enter
+
+echo "Create custom object"
+echo ""
+echo "kubectl apply -f $FULLPATH/set1811-2-website-kubia.yaml"
+kubectl apply -f $FULLPATH/set1811-2-website-kubia.yaml
+
+enter
+
+echo "kubectl get website kubia -n=chp18-set1811 -o yaml"
+kubectl get website kubia -n=chp18-set1811 -o yaml
+
+enter
+
+echo "kubectl delete website kubia -n=chp18-set1811"
+kubectl delete website kubia -n=chp18-set1811
+
+enter
+
+echo "kubectl delete -f $FULLPATH"
+kubectl delete -f $FULLPATH --ignore-not-found
+
