e30a3c82485a38ee7e49508a69876f794cea5622
George Baptista <george@omame.com>
2020-10-19 22:29:53 +0900

    set1812-rbac-crd-controller

--- chp18-extending-k8s/set1812-rbac-crd-controller/TEST.sh
+++ chp18-extending-k8s/set1812-rbac-crd-controller/TEST.sh
@@ -6,45 +6,46 @@ FULLPATH=$(pwd)
 
 echo "kubectl apply -f $FULLPATH/set1812-0-ns.yaml"
 kubectl apply -f $FULLPATH/set1812-0-ns.yaml
+kubectl apply -f $FULLPATH/setup-sa.yaml
 echo $HR
 
 
 # Need this order, at least PSP/RBAC before the crd stuff.
 
 echo "kubectl apply -f $FULLPATH/setup-sa.yaml"
-echo "kubectl apply -f $FULLPATH/setup-psp.yaml"
-echo "kubectl apply -f $FULLPATH/setup-role-rolebinding.yaml"
-echo "kubectl apply -f $FULLPATH/setup-clusterrole-clusterrolebinding.yaml"
+#echo "kubectl apply -f $FULLPATH/setup-psp.yaml"
+#echo "kubectl apply -f $FULLPATH/setup-role-rolebinding.yaml"
+#echo "kubectl apply -f $FULLPATH/setup-clusterrole-clusterrolebinding.yaml"
 
 kubectl apply -f $FULLPATH/setup-sa.yaml
-kubectl apply -f $FULLPATH/setup-psp.yaml
-kubectl apply -f $FULLPATH/setup-role-rolebinding.yaml
-kubectl apply -f $FULLPATH/setup-clusterrole-clusterrolebinding.yaml
+#kubectl apply -f $FULLPATH/setup-psp.yaml
+#kubectl apply -f $FULLPATH/setup-role-rolebinding.yaml
+#kubectl apply -f $FULLPATH/setup-clusterrole-clusterrolebinding.yaml
 
-enter
+#enter
 
 echo "kubectl apply -f $FULLPATH/set1812-1-crd-website.yaml"
 kubectl apply -f $FULLPATH/set1812-1-crd-website.yaml
-enter
+#enter
 
 echo "kubectl apply -f $FULLPATH/set1812-2-website-controller.yaml"
 kubectl apply -f $FULLPATH/set1812-2-website-controller.yaml
-echo $HR
+#echo $HR
 
 echo "kubectl rollout status deployment website-controller -n=chp18-set1812"
 kubectl rollout status deployment website-controller -n=chp18-set1812
-enter
+#enter
 
 echo "kubectl apply -f $FULLPATH/set1812-3-website-kubia.yaml"
 kubectl apply -f $FULLPATH/set1812-3-website-kubia.yaml
+#enter
+
+#kubectl apply -f $FULLPATH
 enter
 
-kubectl apply -f $FULLPATH
-enter
-
-echo "kubectl get website kubia -n=chp18-set1812 -o yaml"
-kubectl get website kubia -n=chp18-set1812 -o yaml
-echo $HR
+#echo "kubectl get website kubia -n=chp18-set1812 -o yaml"
+#kubectl get website -n=chp18-set1812 -o yaml
+#echo $HR
 
 echo "kubectl get all -n=chp18-set1812"
 kubectl get all -n=chp18-set1812
--- chp18-extending-k8s/set1812-rbac-crd-controller/WATCH.sh
+++ chp18-extending-k8s/set1812-rbac-crd-controller/WATCH.sh
@@ -4,24 +4,22 @@ clear
 HR=$(printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -)
 HR_TOP=$(printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' "=")
 
+NODE_IP=$(kubectl get node -o jsonpath={'.items[0].status.addresses[0].address'})
+SITE_IP="http://$NODE_IP:30007"
+
 watch -n 1 -d -t "echo $HR_TOP; \
-kubectl top node; \
+echo "Site deployed at " $SITE_IP; \
 echo $HR; \
-kubectl get nodes -o=custom-columns=NAME:.metadata.name,TAINTS:.spec.taints,LABEL-NAME:.metadata.labels.name,LABEL-ENV:.metadata.labels.env; \
-echo $HR; \
-kubectl top pod -n=chp18-set1812; \
-echo $HR; \
-echo "get website kubia"; \
-kubectl get website kubia -n=chp18-set1812; \
-echo $HR; \
-echo "SERVICE:"; \
+echo "crd, controller, website, svc"; \
+kubectl get crd -n=chp18-set1812 -o wide; \
+kubectl get deployment/website-controller -n=chp18-set1812; \
+kubectl get websites -n=chp18-set1812 -o wide; \
 kubectl get svc -n=chp18-set1812 -o wide; \
 echo $HR; \
-echo "get deployment"; \
+echo "deployment"; \
 kubectl get deployment -n=chp18-set1812 -o wide; \
 echo $HR; \
-echo "get pods"; \
-kubectl get pods -n=chp18-set1812; \
+kubectl get pods -n=chp18-set1812 -o wide; \
 echo $HR; \
 kubectl get events -n=chp18-set1812 -o custom-columns=LastSeen:.lastTimestamp,From:.source.component,Reason:.reason,Message:.message --sort-by=lastTimestamp | tac; \
 echo $HR_TOP"
