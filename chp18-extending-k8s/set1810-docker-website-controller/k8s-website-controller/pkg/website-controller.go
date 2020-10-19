af2614b8e27d369fc5ee6fde63854f691b70c8ec
George Baptista <george@omame.com>
2020-10-19 21:59:58 +0900

    set1810-docker-website-controller

--- chp18-extending-k8s/set1810-docker-website-controller/k8s-website-controller/pkg/website-controller.go
+++ chp18-extending-k8s/set1810-docker-website-controller/k8s-website-controller/pkg/website-controller.go
@@ -1,19 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"encoding/json"
+	"fmt"
 	"io"
-	"log"
-	//"github.com/luksa/website-controller/pkg/v1"
-  "github.com/gbih/k8s-website-controller/pkg/v1"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"strings"
 )
 
+//--------------
+
+type Metadata struct {
+	Name      string
+	Namespace string
+}
+
+type WebsiteSpec struct {
+	GitRepo string
+}
+
+type Website struct {
+	Metadata Metadata
+	Spec     WebsiteSpec
+}
+
+type WebsiteWatchEvent struct {
+	Type   string
+	Object Website
+}
+
+//---------------
+
 func main() {
 	log.Println("website-controller started.")
+
 	for {
 		resp, err := http.Get("http://localhost:8001/apis/extensions.example.com/v1/websites?watch=true")
 		if err != nil {
@@ -23,14 +45,16 @@ func main() {
 
 		decoder := json.NewDecoder(resp.Body)
 		for {
-			var event v1.WebsiteWatchEvent
+			var event WebsiteWatchEvent
 			if err := decoder.Decode(&event); err == io.EOF {
 				break
 			} else if err != nil {
 				log.Fatal(err)
 			}
 
-			log.Printf("Received watch event: %s: %s: %s\n", event.Type, event.Object.Metadata.Name, event.Object.Spec.GitRepo)
+			// log.Printf("Received watch event: Type: %s, Name: %s, GitRepo: %s\n", event.Type, event.Object.Metadata.Name,  event.Object.Spec.GitRepo)
+
+			log.Printf("Debug: event object: %#v\n", event)
 
 			if event.Type == "ADDED" {
 				createWebsite(event.Object)
@@ -42,17 +66,20 @@ func main() {
 
 }
 
-func createWebsite(website v1.Website) {
+func createWebsite(website Website) {
 	createResource(website, "api/v1", "services", "service-template.json")
 	createResource(website, "apis/apps/v1", "deployments", "deployment-template.json")
 }
 
-func deleteWebsite(website v1.Website) {
-	deleteResource(website, "api/v1", "services", getName(website));
-	deleteResource(website, "apis/apps/v1", "deployments", getName(website));
+func deleteWebsite(website Website) {
+	deleteResource(website, "api/v1", "services", getName(website))
+	deleteResource(website, "apis/apps/v1", "deployments", getName(website))
 }
 
-func createResource(webserver v1.Website, apiGroup string, kind string, filename string) {
+func createResource(webserver Website, apiGroup string, kind string, filename string) {
+
+	log.Printf("Namespace: %s", webserver.Metadata.Namespace)
+
 	log.Printf("Creating %s with name %s in namespace %s", kind, getName(webserver), webserver.Metadata.Namespace)
 	templateBytes, err := ioutil.ReadFile(filename)
 	if err != nil {
@@ -61,14 +88,26 @@ func createResource(webserver v1.Website, apiGroup string, kind string, filename
 	template := strings.Replace(string(templateBytes), "[NAME]", getName(webserver), -1)
 	template = strings.Replace(template, "[GIT-REPO]", webserver.Spec.GitRepo, -1)
 
-	resp, err := http.Post(fmt.Sprintf("http://localhost:8001/%s/namespaces/%s/%s/", apiGroup, webserver.Metadata.Namespace, kind), "application/json", strings.NewReader(template))
+	template = strings.Replace(template, "[NAMESPACE]", webserver.Metadata.Namespace, -1)
+
+	query := (fmt.Sprintf("http://localhost:8001/%s/namespaces/%s/%s/", apiGroup, webserver.Metadata.Namespace, kind))
+	log.Println("query: ", query)
+
+	// for debugging
+	queryPost := fmt.Sprint(query, "application/json", strings.NewReader(template))
+	log.Println("queryPost: ", queryPost)
+
+	resp, err := http.Post(query, "application/json", strings.NewReader(template))
+
+	// resp, err := http.Post(fmt.Sprintf("http://localhost:8001/%s/namespaces/%s/%s/", apiGroup, webserver.Metadata.Namespace, kind), "application/json", strings.NewReader(template))
 	if err != nil {
+		fmt.Println("ERROR in createResource()")
 		log.Fatal(err)
 	}
 	log.Println("response Status:", resp.Status)
 }
 
-func deleteResource(webserver v1.Website, apiGroup string, kind string, name string) {
+func deleteResource(webserver Website, apiGroup string, kind string, name string) {
 	log.Printf("Deleting %s with name %s in namespace %s", kind, name, webserver.Metadata.Namespace)
 	req, err := http.NewRequest(http.MethodDelete, fmt.Sprintf("http://localhost:8001/%s/namespaces/%s/%s/%s", apiGroup, webserver.Metadata.Namespace, kind, name), nil)
 	if err != nil {
@@ -84,6 +123,6 @@ func deleteResource(webserver v1.Website, apiGroup string, kind string, name str
 
 }
 
-func getName(website v1.Website) string {
-	return website.Metadata.Name + "-website";
+func getName(website Website) string {
+	return website.Metadata.Name + "-website"
 }
