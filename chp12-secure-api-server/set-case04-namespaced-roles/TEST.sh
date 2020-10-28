bd18d708c69c2550f8812f15bdec9d38e699d5cf
George Baptista <george@omame.com>
2020-10-28 22:21:33 +0900

    chp12

--- chp12-secure-api-server/set-case04-namespaced-roles/TEST.sh
+++ chp12-secure-api-server/set-case04-namespaced-roles/TEST.sh
@@ -7,28 +7,32 @@ echo $HR_TOP
 function urlTest() {
 
   echo "CURL" 
-#  echo "kubectl exec -it my-pod -n chp12-set1227 -- curl localhost:8001/api/v1/componentstatuses"
-  #kubectl exec -it my-pod -- curl localhost:8001/api/v1/componentstatuses
-  # kubectl exec -it my-pod --n=chp12-set1227 -- curl http://localhost:8001/apis/storage.k8s.io/v1/storageclasses
-  # kubectl exec -it my-pod --n=chp12-set1227 -- curl http://localhost:8001/apis/storage.k8s.io/v1/volumeattachments
-  # kubectl exec -it my-pod --n=chp12-set1227 -- curl http://localhost:8001/api/v1/persistentvolumes
-  # kubectl exec -it my-pod --n=chp12-set1227 -- curl http://localhost:8001/apis/admissionregistration.k8s.io/v1/validatingwebhookconfigurations
-   kubectl exec -it my-pod -n=chp12-set1227 -- curl localhost:8001/apis/rbac.authorization.k8s.io/v1/namespaces/chp12-set1227/roles
+   kubectl exec -it my-pod -n=chp12-set1227 -- curl localhost:8001/api/v1/namespaces/chp12-set1227/nodes
 
-# .[]? is similar to .[], but no errors will be output if . is not an array or object.
-  #echo "kubectl exec -it my-pod -n chp12-set1227 -- curl localhost:8001/apis/apps/v1/deployments | jq -r '.items[]?.metadata.name'"
-  #kubectl exec -it my-pod -n chp12-set1227 -- curl localhost:8001/apis/apps/v1/deployments | jq -r '.items[]?.metadata.name'
-
+  echo ""
 }
 
 ###########################################
 
 # Make sure any rolebindings are deleted beforehand
 kubectl delete rolebinding my-binding -n=chp12-set1227 --ignore-not-found
-#kubectl delete rolebinding my-binding -n=chp12-set1227 -n chp12-set1227 --ignore-not-found
+kubectl delete role my-role -n=chp12-set1227 --ignore-not-found
 
 kubectl apply -f $FULLPATH/set1227-0-ns.yaml
 kubectl apply -f $FULLPATH/set1227-1-sa.yaml
+
+
+
+#ROLE
+kubectl create role my-role --verb=get --verb=list --resource=nodes -n chp12-set1227 --dry-run=client -o yaml > my-role.yaml
+
+kubectl apply -f $FULLPATH/my-role.yaml
+#sleep 1
+
+#kubectl create role my-role --verb=get --verb=list --resource=pods -n chp12-set1227
+
+
+
 #kubectl apply -f $FULLPATH/set1227-2-pod.yaml
 kubectl apply -f $FULLPATH/set1227-3-pod-sa.yaml
 #kubectl apply -f $FULLPATH/tokenreview-rolebinding.yaml
@@ -38,6 +42,8 @@ echo "kubectl wait --for=condition=Ready=True pod/my-pod -n=chp12-set1227 --time
 kubectl wait --for=condition=Ready=True pod/my-pod -n=chp12-set1227 --timeout=20s
 
 
+
+#kubectl create role myrole --verb=get --verb=list --resource=configmaps -n chp12-set1227
 
 # Start at root-level permissions, then work out how to lock it down iteratively
 # OK: his gives all service accounts (and implicitly pods) cluster-admin privileges
@@ -49,11 +55,11 @@ kubectl wait --for=condition=Ready=True pod/my-pod -n=chp12-set1227 --timeout=20
 # OK: Gives cluster-admin privileges to service account my-sa in namespace chp12-set1227
 #kubectl create rolebinding my-binding -n=chp12-set1227 --clusterrole=cluster-admin --serviceaccount=chp12-set1227:my-sa --dry-run=client -o yaml > rb-1.yaml
 
-kubectl create rolebinding my-binding -n=chp12-set1227 --clusterrole=admin --serviceaccount=chp12-set1227:my-sa --dry-run=client -o yaml > rb-1.yaml
+#kubectl create rolebinding my-binding -n=chp12-set1227 --clusterrole=admin --serviceaccount=chp12-set1227:my-sa --dry-run=client -o yaml > rb-1.yaml
 
 #kubectl create rolebinding my-binding -n=chp12-set1227 --clusterrole=edit --serviceaccount=chp12-set1227:my-sa --dry-run=client -o yaml > rb-1.yaml
 
-#kubectl create rolebinding my-binding -n=chp12-set1227 --clusterrole=view --serviceaccount=chp12-set1227:my-sa --dry-run=client -o yaml > rb-1.yaml
+kubectl create rolebinding my-binding -n=chp12-set1227 --role=my-role --serviceaccount=chp12-set1227:my-sa --dry-run=client -o yaml > rb-1.yaml
 
 #echo ""
 #value=$(<rb-1.yaml)
@@ -68,8 +74,8 @@ echo ""
 urlTest
 echo $HR
 
-echo "kubectl delete rolebinding my-binding -n=chp12-set1227"
-kubectl delete rolebinding my-binding -n=chp12-set1227
+#echo "kubectl delete rolebinding my-binding -n=chp12-set1227"
+#kubectl delete rolebinding my-binding -n=chp12-set1227
 
 ###########################################
 
